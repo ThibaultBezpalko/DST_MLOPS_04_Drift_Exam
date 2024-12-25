3c488a65ba54f54836d052319898cfe7b3ef4abe
Ubuntu <ubuntu@ip-172-31-0-69.eu-west-1.compute.internal>
2024-12-25 18:05:50 +0000

    Generation of reports with tag and metadata

--- drift_exam.py
+++ drift_exam.py
@@ -38,9 +38,6 @@ def _process_data(raw_data: pd.DataFrame) -> pd.DataFrame:
     return raw_data
 
 
-
-
-
 ############################
 ## HTML REPORT GENERATION
 ############################
@@ -67,58 +64,6 @@ def _process_data(raw_data: pd.DataFrame) -> pd.DataFrame:
 
 # # in a notebook run :
 # # data_drift_dataset_report.show()
-
-
-############################
-## ANALYSE DERIVE - 01
-############################
-
-# # Train the production model
-# regressor.fit(reference_jan11[numerical_features + categorical_features], reference_jan11[target])
-
-# # Perform column mapping
-# column_mapping = ColumnMapping()
-# column_mapping.target = target
-# column_mapping.prediction = prediction
-# column_mapping.numerical_features = numerical_features
-# column_mapping.categorical_features = categorical_features
-
-# # Generate predictions for the reference data
-# ref_prediction = regressor.predict(reference_jan11[numerical_features + categorical_features])
-# reference_jan11['prediction'] = ref_prediction
-
-# # Initialize the regression performance report with the default regression metrics preset
-# regression_report = Report(metrics=[
-#     RegressionPreset(),
-# ])
-
-# # Run the regression performance report using the reference data
-# regression_report.run(reference_data=None, 
-#                                   current_data=reference_jan11,
-#                                   column_mapping=column_mapping)
-
-
-############################
-## ANALYSE DERIVE - 02
-############################
-
-# column_mapping_drift = ColumnMapping()
-
-# column_mapping_drift.target = target
-# column_mapping_drift.prediction = prediction
-# column_mapping_drift.numerical_features = numerical_features
-# column_mapping_drift.categorical_features = []
-
-# data_drift_report = Report(metrics=[
-#     DataDriftPreset(),
-# ])
-
-# data_drift_report.run(
-#     reference_data=reference_jan11,
-#     current_data=current_feb11.loc['2011-02-14 00:00:00':'2011-02-21 23:00:00'],
-#     column_mapping=column_mapping_drift,
-# )
-
 
 
 ############################
@@ -151,12 +96,19 @@ def add_report_to_workspace(workspace, project_name, project_description, report
 ## MAIN
 ############################
 
+# Common data drift metric
+# DatasetDriftMetric(),
+# A line of analysis for each column
+# DataDriftTable(),    
+
+
 if __name__ == "__main__":
     WORKSPACE_NAME = "datascientest-workspace"
     PROJECT_NAME = "drift_monitoring_exam"
     PROJECT_DESCRIPTION = "Evidently Dashboards"
 
     # load data
+    print("=== Loading data ===")
     raw_data = _process_data(_fetch_data())
 
     ############################
@@ -174,6 +126,7 @@ if __name__ == "__main__":
     current_feb11 = raw_data.loc['2011-01-29 00:00:00':'2011-02-28 23:00:00']
 
     # Train test split ONLY on reference_jan11
+    print("=== Train, test, split on January data ===")
     X_train, X_test, y_train, y_test = model_selection.train_test_split(
         reference_jan11[numerical_features + categorical_features],
         reference_jan11[target],
@@ -181,6 +134,7 @@ if __name__ == "__main__":
     )
 
     # Model training
+    print("=== Fitting the random forest regressor on train data ===")
     regressor = ensemble.RandomForestRegressor(random_state = 0, n_estimators = 50)
     regressor.fit(X_train, y_train)
 
@@ -202,43 +156,296 @@ if __name__ == "__main__":
     X_test['prediction'] = preds_test
 
     # Initialize the column mapping object which evidently uses to know how the data is structured
-    column_mapping = ColumnMapping()
+    column_mapping_01 = ColumnMapping()
 
     # Map the actual target and prediction column names in the dataset for evidently
-    column_mapping.target = 'target'
-    column_mapping.prediction = 'prediction'
+    column_mapping_01.target = 'target'
+    column_mapping_01.prediction = 'prediction'
 
     # Specify which features are numerical and which are categorical for the evidently report
-    column_mapping.numerical_features = numerical_features
-    column_mapping.categorical_features = categorical_features
+    column_mapping_01.numerical_features = numerical_features
+    column_mapping_01.categorical_features = categorical_features
+
+    # Generate a tag for the bunch of reports
+    tag = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S") # Example: '2024-12-25T14:30:15'
+    print(f'=== Generation tag: {tag} ===')  
 
     # Initialize the regression performance report with the default regression metrics preset
-    regression_report = Report(metrics=[
-        RegressionPreset(),
-    ])
+    validation_report_01 = Report(
+        metrics=[
+            RegressionPreset(),
+        ],
+        metadata = {
+            "name": "drift_exam_01_model_validation",
+        },
+        tags=[tag],
+    )
 
     # Run the regression performance report using the training data as reference and test data as current
     # The data is sorted by index to ensure consistent ordering for the comparison
-    regression_report.run(reference_data=X_train.sort_index(), 
+    print("=== Running model validation report on January data ===")
+    validation_report_01.run(reference_data=X_train.sort_index(), 
                                     current_data=X_test.sort_index(),
-                                    column_mapping=column_mapping)
+                                    column_mapping=column_mapping_01)
+
 
     # Convert the JSON string to a Python dictionary for pretty printing
-    report_data = json.loads(regression_report.json())
+    report_data_01 = json.loads(validation_report_01.json())
 
     # Save the report in JSON format with indentation for better readability
-    with open('data_drift_report.json', 'w') as f:
-        json.dump(report_data, f, indent=4)
+    with open('drift_exam_01_model_validation.json', 'w') as f:
+        json.dump(report_data_01, f, indent=4)
 
     # save HTML
-    regression_report.save_html("Classification Report.html")
+    validation_report_01.save_html("drift_exam_01_model_validation.html")
 
     # # Generate the regression performance report
     # regression_report = generate_regression_report(reference_data, current_data)
 
     # Create and Add report to workspace
     workspace = Workspace.create(WORKSPACE_NAME)
-    add_report_to_workspace(workspace, PROJECT_NAME, PROJECT_DESCRIPTION, regression_report)
+    add_report_to_workspace(workspace, PROJECT_NAME, PROJECT_DESCRIPTION, validation_report_01)
+
+    
+    ##########################################
+    ## ANALYSE DERIVE MODELE - Janvier
+    ##########################################
+
+    # Train the production model
+    print("=== Training model on complete January data ===")
+    regressor.fit(reference_jan11[numerical_features + categorical_features], reference_jan11[target])
+
+    # Perform column mapping
+    column_mapping = ColumnMapping()
+    column_mapping.target = target
+    column_mapping.prediction = prediction
+    column_mapping.numerical_features = numerical_features
+    column_mapping.categorical_features = categorical_features
+
+    # Generate predictions for the reference data
+    ref_prediction_jan11 = regressor.predict(reference_jan11[numerical_features + categorical_features])
+    reference_jan11['prediction'] = ref_prediction_jan11
+
+    # Initialize the regression performance report with the default regression metrics preset
+    production_report_02 = Report(
+        metrics=[
+            RegressionPreset(),
+        ],
+        metadata = {
+            "name": "drift_exam_02_model_drift_jan",
+        },
+        tags=[tag],
+    )
+
+    # Run the regression performance report using the reference data
+    print("=== Running model drift report on January data ===")
+    production_report_02.run(reference_data=None, 
+                                    current_data=reference_jan11,
+                                    column_mapping=column_mapping)
+
+    # Convert the JSON string to a Python dictionary for pretty printing
+    report_data_02 = json.loads(production_report_02.json())
+
+    # Save the report in JSON format with indentation for better readability
+    with open('drift_exam_02_model_drift_jan.json', 'w') as f:
+        json.dump(report_data_02, f, indent=4)
+
+    # save HTML
+    production_report_02.save_html("drift_exam_02_model_drift_jan.html")
+
+    # # Generate the regression performance report
+    # regression_report = generate_regression_report(reference_data, current_data)
+
+    # Create and Add report to workspace
+    workspace = Workspace.create(WORKSPACE_NAME)
+    add_report_to_workspace(workspace, PROJECT_NAME, PROJECT_DESCRIPTION, production_report_02)
+
+
+
+    ##############################################
+    ## ANALYSE DERIVE MODELE - Février Week 1
+    ##############################################
+
+    # Generate predictions for the current data
+    print("=== Predicting on February 1st week data ===")
+    reference_feb11_01 = current_feb11.loc['2011-01-28 00:00:00':'2011-02-21 23:00:00']
+    ref_prediction_feb11_01 = regressor.predict(reference_feb11_01[numerical_features + categorical_features])
+    reference_feb11_01['prediction'] = ref_prediction_feb11_01
+
+    # Initialize the regression performance report with the default regression metrics preset
+    production_report_03 = Report(
+        metrics=[
+            RegressionPreset(),
+        ],
+        metadata = {
+            "name": "drift_exam_03_model_drift_feb_01",
+        },
+        tags=[tag],
+    )
+
+    # Run the regression performance report using the reference data
+    print("=== Running model drift report on February 1st week data ===")
+    production_report_03.run(reference_data=reference_jan11, 
+                                    current_data=reference_feb11_01,
+                                    column_mapping=column_mapping)
+
+    # Convert the JSON string to a Python dictionary for pretty printing
+    report_data_03 = json.loads(production_report_03.json())
+
+    # Save the report in JSON format with indentation for better readability
+    with open('drift_exam_03_model_drift_feb_01.json', 'w') as f:
+        json.dump(report_data_03, f, indent=4)
+
+    # save HTML
+    production_report_03.save_html("drift_exam_03_model_drift_feb_01.html")
+
+    # # Generate the regression performance report
+    # regression_report = generate_regression_report(reference_data, current_data)
+
+    # Create and Add report to workspace
+    workspace = Workspace.create(WORKSPACE_NAME)
+    add_report_to_workspace(workspace, PROJECT_NAME, PROJECT_DESCRIPTION, production_report_03)
+
+
+    ##############################################
+    ## ANALYSE DERIVE MODELE - Février Week 2
+    ##############################################
+
+    # Generate predictions for the current data
+    print("=== Predicting on February 2nd week data ===")
+    reference_feb11_02= current_feb11.loc['2011-02-08 00:00:00':'2011-02-14 23:00:00']
+    ref_prediction_feb11_02 = regressor.predict(reference_feb11_02[numerical_features + categorical_features])
+    reference_feb11_02['prediction'] = ref_prediction_feb11_02
+
+    # Initialize the regression performance report with the default regression metrics preset
+    production_report_04 = Report(
+        metrics=[
+            RegressionPreset(),
+        ],
+        metadata = {
+            "name": "drift_exam_04_model_drift_feb_02",
+        },
+        tags=[tag],
+    )
+
+    # Run the regression performance report using the reference data
+    print("=== Running model drift report on February 2nd week data ===")
+    production_report_04.run(reference_data=reference_jan11, 
+                                    current_data=reference_feb11_01,
+                                    column_mapping=column_mapping)
+
+    # Convert the JSON string to a Python dictionary for pretty printing
+    report_data_04 = json.loads(production_report_04.json())
+
+    # Save the report in JSON format with indentation for better readability
+    with open('drift_exam_04_model_drift_feb_02.json', 'w') as f:
+        json.dump(report_data_04, f, indent=4)
+
+    # save HTML
+    production_report_04.save_html("drift_exam_04_model_drift_feb_02.html")
+
+    # # Generate the regression performance report
+    # regression_report = generate_regression_report(reference_data, current_data)
+
+    # Create and Add report to workspace
+    workspace = Workspace.create(WORKSPACE_NAME)
+    add_report_to_workspace(workspace, PROJECT_NAME, PROJECT_DESCRIPTION, production_report_04)
+
+
+    ##############################################
+    ## ANALYSE DERIVE MODELE - Février Week 3
+    ##############################################
+
+    # Generate predictions for the current data
+    print("=== Predicting on February 3rd week data ===")
+    reference_feb11_03= current_feb11.loc['2011-02-15 00:00:00':'2011-02-21 23:00:00']
+    ref_prediction_feb11_03 = regressor.predict(reference_feb11_03[numerical_features + categorical_features])
+    reference_feb11_03['prediction'] = ref_prediction_feb11_03
+
+    # Initialize the regression performance report with the default regression metrics preset
+    production_report_05 = Report(
+        metrics=[
+            RegressionPreset(),
+        ],
+        metadata = {
+            "name": "drift_exam_05_model_drift_feb_03",
+        },
+        tags=[tag],
+    )
+
+    # Run the regression performance report using the reference data
+    print("=== Running model drift report on February 3rd week data ===")
+    production_report_05.run(reference_data=reference_jan11, 
+                                    current_data=reference_feb11_01,
+                                    column_mapping=column_mapping)
+
+    # Convert the JSON string to a Python dictionary for pretty printing
+    report_data_05 = json.loads(production_report_05.json())
+
+    # Save the report in JSON format with indentation for better readability
+    with open('drift_exam_05_model_drift_feb_03.json', 'w') as f:
+        json.dump(report_data_05, f, indent=4)
+
+    # save HTML
+    production_report_05.save_html("drift_exam_05_model_drift_feb_03.html")
+
+    # # Generate the regression performance report
+    # regression_report = generate_regression_report(reference_data, current_data)
+
+    # Create and Add report to workspace
+    workspace = Workspace.create(WORKSPACE_NAME)
+    add_report_to_workspace(workspace, PROJECT_NAME, PROJECT_DESCRIPTION, production_report_05)
+
+
+    
+    ######################################################
+    ## ANALYSE DERIVE DONNEES - Février Week 3
+    ######################################################
+
+    # Perform column mapping
+    column_mapping_drift = ColumnMapping()
+
+    column_mapping_drift.target = target
+    column_mapping_drift.prediction = prediction
+    column_mapping_drift.numerical_features = numerical_features
+    column_mapping_drift.categorical_features = []
+
+    # Initialize the data drift report
+    data_drift_report_07 = Report(
+        metrics=[
+            DataDriftPreset(),
+        ],
+        metadata = {
+            "name": "drift_exam_07_data_drift_feb_03",
+        },
+        tags=[tag],
+    )
+
+    print("=== Running data drift report on February 3rd week data ===")
+    data_drift_report_07.run(
+        reference_data=reference_jan11,
+        current_data=reference_feb11_03,
+        column_mapping=column_mapping_drift,
+    )
+
+    # Convert the JSON string to a Python dictionary for pretty printing
+    report_data_07 = json.loads(data_drift_report_07.json())
+
+    # Save the report in JSON format with indentation for better readability
+    with open('drift_exam_07_data_drift_feb_03.json', 'w') as f:
+        json.dump(report_data_07, f, indent=4)
+
+    # save HTML
+    data_drift_report_07.save_html("drift_exam_07_data_drift_feb_03.html")
+
+    # # Generate the regression performance report
+    # regression_report = generate_regression_report(reference_data, current_data)
+
+    # Create and Add report to workspace
+    workspace = Workspace.create(WORKSPACE_NAME)
+    add_report_to_workspace(workspace, PROJECT_NAME, PROJECT_DESCRIPTION, data_drift_report_07)
+
+
 
     # Visualize the report in evidently UI at the URL http://localhost:8000
     # evidently ui --workspace ./datascientest-workspace/
